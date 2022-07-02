63e9fc8d38c36254a839b3ac05c10b24dc23ba64
FR Ranad <frranad2@gmail.com>
2022-07-03 01:26:17 +0600

    Associative Array

---
+++ associative-array.php
@@ -0,0 +1,9 @@
+<?php
+// Associative Array
+$array_via_function = array('value1' => 10, 'value2' => 20, 'value3' => 30);
+$array_via_square_braces = ['value1' => 10, 'value2' => 20, 'value3' => 30];
+echo '<pre>';
+print_r($array_via_square_braces);
+echo '</pre>';
+
+?>
