44e2462069c88507311f17969fa9fd36750b7527
FR Ranad <frranad2@gmail.com>
2022-07-03 01:04:34 +0600

    Basic array print with []

--- array.php
+++ array.php
@@ -1,7 +1,8 @@
 <?php
 $array_via_function = array('value1', 'value2', 'value3');
+$array_via_square_braces = array('value1', 'value2', 'value3');
 echo '<pre>';
-print_r($array_via_function);
+print_r($array_via_square_braces);
 echo '</pre>';
 
 ?>
